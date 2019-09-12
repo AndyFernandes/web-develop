025ba30f0eb8aa8b58f68b14104c4157afd3c34e
andyfernandes <andrezafernandes@alu.ufc.br>
2019-09-12 17:38:34 -0300

    [+] Inicio atividade 3 + Html completo

---
+++ atividade3/js/script.js
@@ -0,0 +1,33 @@
+// vetor pra armazenar objeto aluno {'matricula', 'nome, 'ddd', 'telefone', 'operadora', 'campus', 'curso'}
+
+// click pro ? que será about
+
+// campos obrigatórios do forms
+
+// limpar dados do forms
+
+// tabelas atualizam quando insere um novo aluno -> tabela ordenada
+
+// botão pra remoção de aluno na tabela
+
+// não é permitido inclusão de aluno com mesma matrícula
+
+// paginação na tabela de usuários
+
+// armazenar campus e curso
+
+// validação de matricula (6 num)
+
+// validação de ddd e telefone
+
+// validação email + nome + data nasc
+
+// permitir alteração das info de alunos ->  
+
+alunos = []
+
+campi_cursos = {
+    'Porangabussu': ['Medicina', 'Odontologia', 'Farmácia'],
+    'Pici': ['Computação', 'Matemática', 'Geologia'],
+    'Benfica': ['Letras', 'Filosofia', 'Direito']
+}
