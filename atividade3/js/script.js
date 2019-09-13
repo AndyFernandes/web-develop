0786ce97d759357e3ec343d42a1162735abe3798
andyfernandes <andrezafernandes@alu.ufc.br>
2019-09-13 15:34:57 -0300

    [+] Inicio da lógica do inserir + escrita de outras funções

--- atividade3/js/script.js
+++ atividade3/js/script.js
@@ -32,7 +32,7 @@ campi_cursos = {
     'Benfica': ['Letras', 'Filosofia', 'Direito']
 }
 
-$('#remove').modal('show');
+$('#remove').modal('hide');
 
 var selectCampus = document.getElementById('campus');
 
@@ -42,18 +42,74 @@ function validar_ddd(ddd) {}
 
 function validar_telefone(telefone) {}
 
-function validar_email() {}
+function validar_email(email) {}
 
-function validar_nome() {}
+function validar_nome(nome) {}
 
-function validar_data_nasc() {}
+function validar_data_nasc(data_nasc) {}
 
 function changeSelect(evento) {
     console.log(evento.value);
 }
 
-function inserir() {
+function ordenar_alunos(){
 
 }
 
-function limpar() {}
+function atualizar_valores_tabela(){
+
+}
+
+function inserir() {
+    var matricula = document.getElementById('matricula').value;
+    var nome = document.getElementById('nome').value;
+    var data_nasc = document.getElementById('data_nasc').value;
+    var email = document.getElementById('email').value;
+    var ddd = document.getElementById('ddd').value;
+    var telefone = document.getElementById('telefone').value;
+    var operadora = document.getElementById('operadoras').value;
+    var campus = document.getElementById('campus').value;
+    var curso = document.getElementById('curso').value;
+
+    validar_matricula(matricula);
+    validar_nome(nome);
+    validar_data_nasc(data_nasc);
+    validar_email(email);
+    validar_ddd(ddd);
+    validar_telefone(telefone);
+
+    alunos.push({
+        'matricula': matricula,
+        'nome': nome,
+        'data_nasc': data_nasc,
+        'email': email,
+        'ddd': ddd,
+        'telefone': telefone,
+        'operadora': operadora,
+        'campus': campus,
+        'curso': curso
+    });
+    
+    atualizar_valores_tabela();
+    console.log(alunos);
+}
+
+function remover(evento){
+    $('#remove').modal('show');
+
+
+}
+
+function atualizar(evento){}
+
+function limpar() {
+    document.getElementById('matricula').value = '';
+    document.getElementById('nome').value = '';
+    document.getElementById('data_nasc').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('ddd').value = '';
+    document.getElementById('telefone').value = '';
+    document.getElementById('operadoras').value = '';
+    document.getElementById('campus').value = '';
+    document.getElementById('curso').value = '';
+}
