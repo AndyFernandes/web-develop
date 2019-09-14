5c3793d0570a2672a5aa5f939c886f638bdaee7d
andyfernandes <andrezafernandes@alu.ufc.br>
2019-09-14 01:23:56 -0300

    [+] Funções pra adicionar alunos na tabela, validações no js + sombra vermelha nos inputs e etc etc

--- atividade3/js/script.js
+++ atividade3/js/script.js
@@ -14,38 +14,73 @@
 
 // paginação na tabela de usuários
 
-alunos = []
-
+alunos = [];
 campi_cursos = {
     'Porangabussu': ['Medicina', 'Odontologia', 'Farmácia'],
     'Pici': ['Computação', 'Matemática', 'Geologia'],
     'Benfica': ['Letras', 'Filosofia', 'Direito']
-}
-
+};
+var tbody = document.querySelector('tbody');
+console.log(tbody);
 $('#remove').modal('hide');
+
 
 ////////// OPERAÇÕES
 // TODO
-function ordenar_alunos(){}
-// TODO
-function atualizar_valores_tabela(){
-    // remover filhos da tabela
-    // apendar novos filhos na tabela
-    // OU só criar um novo elemento da tabela, pegar os elementos da tabela, e ordenalos
-}
+function ordenar_alunos() {}
+
+function gerar_linha(aluno) {
+    var botao_remover = document.createElement('button');
+    botao_remover.className = 'btn btn-light ' + aluno['matricula'];
+    botao_remover.id = "remover_aluno";
+    botao_remover.value = 'Remover';
+
+    var botao_alterar = document.createElement('button');
+    botao_alterar.className = 'btn btn-light ' + aluno['matricula'];
+    botao_alterar.value = 'Alterar';
+    botao_alterar.id = "alterar_aluno";
+
+    var tr = document.createElement('tr');
+    var th_matricula = document.createElement('th');
+    var th_nome = document.createElement('th');
+    var th_botao = document.createElement('th');
+
+    var matricula = document.createTextNode(aluno['matricula']);
+    var nome = document.createTextNode(aluno['nome']);
+
+    th_matricula.appendChild(matricula);
+    th_nome.appendChild(nome);
+    th_botao.appendChild(botao_remover);
+    th_botao.appendChild(botao_alterar);
+
+    tr.appendChild(th_matricula);
+    tr.appendChild(th_nome);
+    tr.appendChild(th_botao);
+    return tr;
+};
+
+function atualizar_valores_tabela() {
+    var childs = tbody.querySelectorAll('tr');
+    childs.forEach(child => {
+        child.remove();
+    });
+
+    alunos.forEach(aluno => {
+        tbody.appendChild(gerar_linha(aluno));
+    });
+};
 
 ////////////////////// LISTENS
 // TODO
-function next(){} // proxima página 
+function next() {} // proxima página 
 
-function back(){} // voltar pagina
+function back() {} // voltar pagina
 
 function changeSelect(evento) {
     var select = document.getElementById('curso').querySelectorAll('option');
     var curso = evento.value;
-    // console.log(select);
+
     select.forEach((element, index) => {
-        // console.log(element, index);
         element.value = campi_cursos[curso][index];
         element.innerText = campi_cursos[curso][index];
     });
@@ -65,26 +100,35 @@ function limpar() {
 
 //////////////////////// VALIDAÇÕES
 // TODO
-function verificacao_duplicidade_matricula(matricula){}
-
-// TODO
-// validação de matricula (6 num)
-function validar_matricula(matricula) {
-    //// validação aqui
-    verificacao_duplicidade_matricula(matricula);
+function verificacao_duplicidade_matricula(matricula) {
+    alunos.forEach(aluno => {
+        if (aluno.matricula == matricula) return false;
+    });
+    return true;
+    //mostrar modal
 }
 
-// validação de ddd 
-function validar_ddd(ddd) {}
+function validar_matricula(matricula) {
+    if (matricula.length === 6 && !isNaN(matricula) && verificacao_duplicidade_matricula(matricula)) return true;
+    return false;
+}
 
-// validação de telefone
-function validar_telefone(telefone) {}
+function validar_ddd(ddd) {
+    if (ddd.length === 2 && !isNaN(ddd)) return true;
+    return false;
+};
 
-// validação email 
+function validar_telefone(telefone) {
+    if ((telefone.length == 8 || telefone.length == 9) && !isNaN(telefone)) return true;
+    return false;
+}
+
 function validar_email(email) {}
 
-// validação nome 
-function validar_nome(nome) {}
+function validar_nome(nome) {
+    if (nome.length >= 2) return true;
+    return false;
+}
 
 // validação data nascimento 
 function validar_data_nasc(data_nasc) {}
@@ -101,12 +145,14 @@ function inserir() {
     var campus = document.getElementById('campus').value;
     var curso = document.getElementById('curso').value;
 
-    validar_matricula(matricula);
-    validar_nome(nome);
-    validar_data_nasc(data_nasc);
-    validar_email(email);
-    validar_ddd(ddd);
-    validar_telefone(telefone);
+    if (validar_matricula(matricula) &&
+        validar_nome(nome) &&
+        validar_data_nasc(data_nasc) &&
+        validar_email(email) &&
+        validar_ddd(ddd) &&
+        validar_telefone(telefone)) {
+        return;
+    }
 
     alunos.push({
         'matricula': matricula,
@@ -119,14 +165,13 @@ function inserir() {
         'campus': campus,
         'curso': curso
     });
-    
+
     atualizar_valores_tabela();
     console.log(alunos);
 }
 
-function remover(evento){
+function remover(evento) {
     $('#remove').modal('show');
 }
 
-function atualizar(evento){}
-
+function atualizar(evento) {}
